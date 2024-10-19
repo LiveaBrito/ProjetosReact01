ad2f4413d226b63a8bd820f54b6156dd913f626f
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-19 18:29:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Texto from './components/Texto'
+
+function App() {
+
+  return (
+    <>
+      <Texto></Texto>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Texto.jsx
@@ -0,0 +1,6 @@
+
+function Texto(){
+    return <h1>Bem-vindo ao React!</h1>
+}
+
+export default Texto
